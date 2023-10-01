b7439c06d3b738d078985c29b95a5d1163e2c507
lyj2ee <134721892+lyj2ee@users.noreply.github.com>
2023-10-01 18:14:42 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,56 @@
+info.onCountdownEnd(function () {
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroy(终点)
+    info.stopCountdown()
+    tiles.setCurrentTilemap(tilemap`级别2`)
+    info.setScore(0)
+    info.changeScoreBy(100)
+    game.gameOver(true)
+})
+let 终点: Sprite = null
+game.splash("一天早上，", "你发现外面是一个迷宫")
+game.splash("目标", "走出迷宫")
+let 我 = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`级别1`)
+终点 = sprites.create(img`
+    b b b b b b b b b b b b b b b b 
+    b c b e 4 4 4 4 4 4 4 4 e b c b 
+    b b e 4 4 4 4 4 4 4 4 4 4 e b b 
+    b b 4 4 4 4 4 4 4 4 4 4 4 4 b b 
+    b b 4 4 4 4 4 4 4 4 4 4 4 4 b b 
+    b b 4 4 4 4 4 4 4 4 4 4 4 4 b b 
+    b b 4 4 4 4 4 4 4 4 4 4 4 4 b b 
+    b b 4 4 4 4 4 4 4 4 4 4 4 4 b b 
+    b b d 4 4 4 4 4 4 4 4 4 4 d b b 
+    b b d 4 4 4 4 4 4 4 4 4 4 d b b 
+    b b 4 d 4 4 4 4 4 4 4 4 d 4 b b 
+    b b 4 4 d d d d d d d d 4 4 b b 
+    b b c 4 4 4 4 4 4 4 4 4 4 c b b 
+    b b b c c c c c c c c c c b b b 
+    b c b b b b b b b b b b b b c b 
+    b b b b b b b b b b b b b b b b 
+    `, SpriteKind.Player)
+controller.moveSprite(我)
+tiles.placeOnRandomTile(终点, sprites.castle.tilePath5)
+tiles.placeOnRandomTile(我, sprites.castle.tilePath5)
+scene.cameraFollowSprite(我)
+info.startCountdown(30)
